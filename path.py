a89c481186115100db3277a28ce6b1712f35c89f
Mahan Marwat <mahanmarwat@users.noreply.github.com>
2014-10-24 05:43:57 +0100

    doc string added

--- path.py
+++ path.py
@@ -1,3 +1,49 @@
+"""
+Path: Path for Pretty People
+============================
+
+path provide highly **readable** API for OS path manipulation and accessing filesystem. All operations are
+available at module level as functions.
+
+    >>> import path
+    >>> path.cwd() # return current working directory.
+    >>> path.chd() # change current working directory.
+    ...
+    
+Operations are available as `Path` object methods too.
+
+    >>> p = path.Path('c:\\hello\world.py')
+    >>> p.name
+    'world'
+    >>> p.ext
+    '.py'
+    >>> p.ispath(path.EXIST) # check path or file existence.
+    >>> p.isempty() # whether the file or dir is empty.
+    ...
+    
+Individual classes for readability.
+
+    >>> comp = path.Components('c:\\hello\world.py')
+    >>> comp.name
+    'world'
+    >>> comp.ext
+    '.py'
+    
+Unlike other packages, path also work on **URL** too. Which ease web-scrapper coding.
+
+    >>> p = path.HttpPath('https://github.com/docopt/docopt')
+    >>> p.path
+    '/docopt/docopt'
+    >>> p.create() # will create dir or file according to args.
+    ...
+
+Author: Mahan Marwat <mahanmarwat@gmail.com>
+Path repo: https://github.com/mahanmarwat/path
+
+:copyright: (c) 2014 by Mahan Marwat.
+:license: MIT, see LICENSE for more details.
+"""
+
 import re, os, sys
 import stat
 import shutil
