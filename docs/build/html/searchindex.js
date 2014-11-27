7e633bb32239a5b663f390e1fd7e654ca4eac576
Mahan Marwat <mahanmarwat@gmail.com>
2014-11-27 04:13:55 +0000

    docs dir added

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({filenames:["index"],titles:["Welcome to Path&#8217;s documentation!"],objnames:{},objects:{},envversion:43,titleterms:{path:0,welcom:0,tabl:0,indic:0,document:0},objtypes:{},terms:{page:0,index:0,content:0,modul:0,search:0}})
